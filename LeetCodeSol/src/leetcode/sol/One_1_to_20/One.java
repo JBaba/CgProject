e588c710a11c9adc3e68463639722d2a73d73828
JBaba <naimish101.viradia@gmail.com>
2016-06-24 00:42:21 -0400

    one two sum added

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/One.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/One.java
@@ -1,10 +1,58 @@
 package leetcode.sol.One_1_to_20;
 
+/**
+ * @author jbaba
+ *
+ *	Given nums = [2, 7, 11, 15], target = 9,
+ *
+ *	Because nums[0] + nums[1] = 2 + 7 = 9,
+ *	return [0, 1].
+ *
+ */
+
 public class One {
+	
+	public int[] twoSum(int[] nums, int target) {
+		int[] indices = new int[2]; 
+		
+		if(nums==null || nums.length < 2)
+			return indices;
+		
+		int sum = nums[0]+nums[1];
+		
+		if(sum==target){
+			indices[0]=0;
+			indices[1]=1;
+			return indices;
+		}
+		
+		for (int i = 2; i < indices.length; i++) {
+			sum = sum - indices[i-2];
+			sum = sum + indices[i];
+			
+			if(sum == target){
+				indices[0]=i-1;
+				indices[1]=i;
+			}
+		}
+		
+		return indices;
+	}
+	
+	public void print(int[] nums){
+		if(nums!=null){
+			System.out.print("Ans: ");
+			for (int i = 0; i < nums.length; i++) {
+				System.out.print(nums[i]+",");
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		One o = new One();
+		int[] nums1 = {2, 7, 11, 15};
+		o.print(o.twoSum(nums1, 9));
 	}
 
 }
