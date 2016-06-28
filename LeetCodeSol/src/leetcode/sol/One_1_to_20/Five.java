44d89eec3b7e6e95610e97cc1d0367f569bca85b
JBaba <naimish101.viradia@gmail.com>
2016-06-28 10:44:00 -0400

    Five Init longest palindromic

---
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/Five.java
@@ -0,0 +1,24 @@
+package leetcode.sol.One_1_to_20;
+
+/**
+ * Given a string S, find the longest palindromic substring in S. 
+ * You may assume that the maximum length of S is 1000, 
+ * and there exists one unique longest palindromic substring.
+ * 
+ * @author nviradia
+ *
+ */
+
+public class Five {
+
+	public String longestPalindrome(String s) {
+		return s;
+        
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
