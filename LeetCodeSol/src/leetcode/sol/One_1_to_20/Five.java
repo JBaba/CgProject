77e89dcc867b3379050ef815e024cc3419028a65
JBaba <naimish101.viradia@gmail.com>
2016-07-08 16:43:41 -0400

    Odd palindrome added

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/Five.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/Five.java
@@ -12,13 +12,37 @@ package leetcode.sol.One_1_to_20;
 public class Five {
 
 	public String longestPalindrome(String s) {
+		
+		String longestPalindrome = "";
+		
+		for(int i=0;i<s.length();i++){
+			int index = isPalindromeOdd(i,s);
+			System.out.println("i:"+i+" in:"+index+" -- "+s.substring(i-index,i+index+1));
+		}
+		
 		return s;
-        
     }
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int isPalindromeOdd(int i, String s) {
+		int left = i,right = i;
+		for(int j=0;j<i;j++){
+			try{
+				char leftChar = s.charAt(left-j);
+				char rightChar = s.charAt(right+j);
+				if(leftChar != rightChar)
+					return j-1;
+			}catch(Exception e){
+				return j-1;
+			}
+		}
+		return i;
+	}
 
+	public static void main(String[] args) {
+
+		Five f = new Five();
+		System.out.println(f.longestPalindrome("abacdgfdcaba"));
+		
 	}
 
 }
