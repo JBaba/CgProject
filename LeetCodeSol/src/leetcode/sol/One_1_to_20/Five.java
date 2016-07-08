24ae9900569780f79358a92b7c4d6aa50edfe959
JBaba <naimish101.viradia@gmail.com>
2016-07-08 18:34:53 -0400

    longest Palindrome impl

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/Five.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/Five.java
@@ -15,6 +15,10 @@ public class Five {
 		
 		String longestPalindrome = "";
 		
+		if(s.length()==1 || s.length()==0 || s.trim().length()==0){
+			return s;
+		}
+		
 		for(int i=0;i<s.length();i++){
 			int index = isPalindromeOdd(i,s);
 			String temp = s.substring(i-index,i+index+1);
@@ -34,12 +38,12 @@ public class Five {
 	
 	private int isPalindromeEven(int i, String s) {
 		int left = i,right = i+1;
-		for(int j=0;j<i;j++){
+		for(int j=0;j<i+1;j++){
 			try{
 				char leftChar = s.charAt(left-j);
 				char rightChar = s.charAt(right+j);
 				if(leftChar != rightChar)
-					return j-1;
+					return j;
 			}catch(Exception e){
 				return j-1;
 			}
@@ -49,12 +53,12 @@ public class Five {
 	
 	private int isPalindromeOdd(int i, String s) {
 		int left = i,right = i;
-		for(int j=0;j<i;j++){
+		for(int j=0;j<i+1;j++){
 			try{
 				char leftChar = s.charAt(left-j);
 				char rightChar = s.charAt(right+j);
 				if(leftChar != rightChar)
-					return j-1;
+					return j;
 			}catch(Exception e){
 				return j-1;
 			}
@@ -65,7 +69,11 @@ public class Five {
 	public static void main(String[] args) {
 
 		Five f = new Five();
-		System.out.println(f.longestPalindrome("aaxa"));
+		System.out.println(f.longestPalindrome("aaaaa"));
+		System.out.println(f.longestPalindrome("aa"));
+		System.out.println(f.longestPalindrome("aaaa"));
+		System.out.println(f.longestPalindrome("aaa"));
+		
 		
 	}
 
