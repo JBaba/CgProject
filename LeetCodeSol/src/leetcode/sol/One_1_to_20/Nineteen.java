9fc0e0e64b598ac84714400cf8f04ee5eab9d14d
JBaba <naimish101.viradia@gmail.com>
2016-07-10 15:18:51 -0400

    Usind Size count

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/Nineteen.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/Nineteen.java
@@ -24,12 +24,40 @@ import leetcode.sol.helper.ListNode;
 public class Nineteen {
 
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		return null;
-        
+		
+		ListNode tempHead = new ListNode(0);
+		tempHead.next = head;
+		
+		ListNode pointer = tempHead;
+		int size = 1;
+		
+		while(pointer.next != null){
+			pointer = pointer.next;
+			size ++;
+		}
+		
+		System.out.println("size:"+size);
+		pointer = tempHead;
+		int index = 0;
+		while(index != (size-n-1)){
+			pointer = pointer.next;
+			index++;
+		}
+		
+		ListNode temp = pointer.next;
+		pointer.next = temp.next;
+		
+		return tempHead.next;
     }
 	
 	public static void main(String[] args) {
-
+		Two t = new Two();
+		ListNode l1 = t.init("12345");
+		System.out.println(l1);
+		
+		Nineteen n = new Nineteen();
+		l1 = n.removeNthFromEnd(l1, 5);
+		System.out.println(l1);
 	}
 
 }
