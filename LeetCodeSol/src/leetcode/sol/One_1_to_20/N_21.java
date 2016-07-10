99b64e8b65d59a297a52e7c062839c2813bdd40d
JBaba <naimish101.viradia@gmail.com>
2016-07-10 15:44:08 -0400

    Q 21 solution excepted

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/N_21.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/N_21.java
@@ -5,12 +5,41 @@ import leetcode.sol.helper.ListNode;
 public class N_21 {
 	
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		return l2;
+		ListNode dummyHead = new ListNode(0);
+		ListNode pointer = dummyHead;
+		
+		ListNode l1Copy = l1;
+		ListNode l2Copy = l2;
+		
+		while(l1Copy!=null &&  l2Copy!=null){
+			int val1 = l1Copy.val;
+			int val2 = l2Copy.val;
+			
+			if(val1>val2){
+				pointer.next = new ListNode(val2);
+				pointer = pointer.next;
+				l2Copy=l2Copy.next;
+			}else{
+				pointer.next = new ListNode(val1);
+				pointer = pointer.next;
+				l1Copy=l1Copy.next;
+			}
+		}
+		
+		if(l1Copy==null){
+			pointer.next = l2Copy;
+		}
+		
+		if(l2Copy==null){
+			pointer.next = l1Copy;
+		}
+		
+		return dummyHead.next;
     }
 
 	public static void main(String[] args) {
 		Two t = new Two();
-		ListNode l1 = t.init("124");
+		ListNode l1 = t.init("1124");
 		System.out.println(l1);
 		
 		ListNode l2 = t.init("356");
