b5e258142f58442d65788c92a88930105e0a4764
JBaba <naimish101.viradia@gmail.com>
2016-07-13 10:35:12 -0400

    Q 10 init

---
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/N_10.java
@@ -0,0 +1,41 @@
+package leetcode.sol.One_1_to_20;
+
+/**
+ * 	Implement regular expression matching with support for '.' and '*'.
+
+	'.' Matches any single character.
+	'*' Matches zero or more of the preceding element.
+	
+	The matching should cover the entire input string (not partial).
+	
+	The function prototype should be:
+	bool isMatch(const char *s, const char *p)
+	
+	Some examples:
+	isMatch("aa","a") → false
+	isMatch("aa","aa") → true
+	isMatch("aaa","aa") → false
+	isMatch("aa", "a*") → true
+	isMatch("aa", ".*") → true
+	isMatch("ab", ".*") → true
+	isMatch("aab", "c*a*b") → true
+ * 
+ * 
+ * @author nviradia
+ *
+ */
+public class N_10 {
+	
+	public N_10() {
+	}
+	
+	public boolean isMatch(String s, String p) {
+        
+		return false;
+    }
+
+	public static void main(String[] args) {
+
+	}
+
+}
