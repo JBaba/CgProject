382e5a1759ea638acc9ec1f3eebc4edfdf508799
JBaba <naimish101.viradia@gmail.com>
2016-07-18 13:26:15 -0400

    Q 10 init

---
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/N_6.java
@@ -0,0 +1,36 @@
+package leetcode.sol.One_1_to_20;
+
+/**
+ * 
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+	P   A   H   N
+	A P L S I I G
+	Y   I   R
+	And then read line by line: "PAHNAPLSIIGYIR"
+	Write the code that will take a string and make this conversion given a number of rows:
+	
+	string convert(string text, int nRows);
+	convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ * 
+ * @author nviradia
+ *
+ */
+public class N_6 {
+
+	public String convert(String s, int numRows) {
+		String ans = "";
+		
+		for(int i=0;i<numRows;i++){
+			
+		}
+		
+		return ans;
+    }
+	
+	public static void main(String[] args) {
+		N_6 n = new N_6();
+		n.convert("PAYPALISHIRING", 3);
+	}
+
+}
