b8a385ede4a1c9d69292ea07c48d15292ab1a744
jbaba <naimish101.viradia@gmail.com>
2016-07-29 19:58:01 -0400

    application msg

---
+++ MultithreadingPoc/src/multi/thread/jdbc/MySqlTest.java
@@ -0,0 +1,30 @@
+package multi.thread.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * My Sql Test connection example
+ * @author jbaba
+ *
+ */
+public class MySqlTest {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/javabase";
+		String username = "java";
+		String password = "password";
+
+		System.out.println("Connecting database...");
+
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+		    System.out.println("Database connected!");
+		} catch (SQLException e) {
+		    throw new IllegalStateException("Cannot connect the database!", e);
+		}
+		
+	}
+
+}
