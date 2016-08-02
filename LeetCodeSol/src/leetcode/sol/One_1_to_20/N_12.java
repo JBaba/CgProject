82de8400dccac6017c3bba3d6ecfda32e0e17856
JBaba <naimish101.viradia@gmail.com>
2016-08-01 22:54:19 -0400

    N 12 init

---
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/N_12.java
@@ -0,0 +1,28 @@
+package leetcode.sol.One_1_to_20;
+
+/**
+ * 
+ * Given an integer, convert it to a roman numeral.
+
+   Input is guaranteed to be within the range from 1 to 3999.
+ * 
+ * Q : https://leetcode.com/problems/integer-to-roman/
+ * 
+ * @author jbaba
+ *
+ */
+public class N_12 {
+	
+	public N_12() {
+	}
+	
+	public String intToRoman(int num) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		N_12 n = new N_12();
+		System.out.println(n.intToRoman(1));
+	}
+
+}
