45853c3b2e6cb2902fe7097a761a67f271035bd9
jbaba <naimish101.viradia@gmail.com>
2016-08-17 21:25:01 -0400

    Tf entity added

---
+++ MultithreadingPoc/src/multi/thread/entity/TfPayment.java
@@ -0,0 +1,170 @@
+package multi.thread.entity;
+
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import multi.thread.jdbc.Entity;
+import multi.thread.logs.ILog;
+
+/**
+ * TfPayment entity for TF_PAYMENT table
+ * @author jbaba
+ *
+ */
+public class TfPayment extends Entity {
+    private BigInteger paymentKey;
+    private String progCd;
+    private String paymentBegDt;
+    private String paymentEndDt;
+    private String createDt;
+    private String userId;
+    private double payAmt;
+    private String processSw;
+    private String caseNum;
+    private String edgNum;
+
+   
+
+    public TfPayment(BigInteger paymentKey, String progCd, String paymentBegDt, String paymentEndDt, String createDt,
+			String userId, double payAmt, String processSw, String caseNum, String edgNum) {
+		super();
+		this.paymentKey = paymentKey;
+		this.progCd = progCd;
+		this.paymentBegDt = paymentBegDt;
+		this.paymentEndDt = paymentEndDt;
+		this.createDt = createDt;
+		this.userId = userId;
+		this.payAmt = payAmt;
+		this.processSw = processSw;
+		this.caseNum = caseNum;
+		this.edgNum = edgNum;
+	}
+
+	public TfPayment(long paymentKey) throws SQLException {
+        super();
+        this.paymentKey = new BigInteger(paymentKey+"");
+        setUserById(paymentKey);
+    }
+
+	/**
+	 * Get record for given payment key
+	 * @param paymentKey
+	 * @throws SQLException
+	 */
+    private void setUserById(long paymentKey) throws SQLException {
+        ResultSet resultSet = getResultSet("SELECT * FROM Tf_Payment WHERE payment_Key = " + paymentKey);
+        if(resultSet.next()) {
+        	this.paymentKey = new BigInteger(resultSet.getString("payment_Key"));
+        	progCd = resultSet.getString("prog_cd");
+        	paymentBegDt = resultSet.getString("payment_beg_dt");
+        	paymentEndDt = resultSet.getString("payment_end_dt");
+            createDt = resultSet.getString("create_dt");
+            userId = resultSet.getString("user_id");
+            payAmt = Double.parseDouble(resultSet.getString("pay_amt"));
+            processSw = resultSet.getString("process_sw");
+            caseNum = resultSet.getString("case_num");
+            edgNum = resultSet.getString("edg_num");
+        }else{
+        	ILog.iclog("No records found with key :"+paymentKey);
+        }
+    }
+
+	@Override
+	public String toString() {
+		return "TfPayment [paymentKey=" + paymentKey + ", progCd=" + progCd + ", paymentBegDt=" + paymentBegDt
+				+ ", paymentEndDt=" + paymentEndDt + ", createDt=" + createDt + ", userId=" + userId + ", payAmt="
+				+ payAmt + ", processSw=" + processSw + ", caseNum=" + caseNum + ", edgNum=" + edgNum + "]";
+	}
+	
+	public static void main(String[] args){
+		try{
+			TfPayment fs = new TfPayment(1);
+			ILog.iclog(fs.toString());
+		}catch(Exception e){
+			ILog.iclog(e);
+		}
+	}
+
+	public BigInteger getPaymentKey() {
+		return paymentKey;
+	}
+
+	public void setPaymentKey(BigInteger paymentKey) {
+		this.paymentKey = paymentKey;
+	}
+
+	public String getProgCd() {
+		return progCd;
+	}
+
+	public void setProgCd(String progCd) {
+		this.progCd = progCd;
+	}
+
+	public String getPaymentBegDt() {
+		return paymentBegDt;
+	}
+
+	public void setPaymentBegDt(String paymentBegDt) {
+		this.paymentBegDt = paymentBegDt;
+	}
+
+	public String getPaymentEndDt() {
+		return paymentEndDt;
+	}
+
+	public void setPaymentEndDt(String paymentEndDt) {
+		this.paymentEndDt = paymentEndDt;
+	}
+
+	public String getCreateDt() {
+		return createDt;
+	}
+
+	public void setCreateDt(String createDt) {
+		this.createDt = createDt;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public double getPayAmt() {
+		return payAmt;
+	}
+
+	public void setPayAmt(double payAmt) {
+		this.payAmt = payAmt;
+	}
+
+	public String getProcessSw() {
+		return processSw;
+	}
+
+	public void setProcessSw(String processSw) {
+		this.processSw = processSw;
+	}
+
+	public String getCaseNum() {
+		return caseNum;
+	}
+
+	public void setCaseNum(String caseNum) {
+		this.caseNum = caseNum;
+	}
+
+	public String getEdgNum() {
+		return edgNum;
+	}
+
+	public void setEdgNum(String edgNum) {
+		this.edgNum = edgNum;
+	}
+
+	
+}
