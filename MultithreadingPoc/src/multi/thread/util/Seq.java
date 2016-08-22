d514476706b1cbf7f3bd87d03d5aa70b24f33df7
jbaba <naimish101.viradia@gmail.com>
2016-08-21 21:51:27 -0400

    seq added

--- MultithreadingPoc/src/multi/thread/util/Seq.java
+++ MultithreadingPoc/src/multi/thread/util/Seq.java
@@ -11,14 +11,26 @@ public class Seq {
 	long pm = 2;
 	long tf = 2;
 	
+	/**
+	 * Seq for FS payments
+	 * @return
+	 */
 	public synchronized long nextFs(){
 		return fs++;
 	}
 	
+	/**
+	 * Seq for PM payments
+	 * @return
+	 */
 	public synchronized long nextPm(){
 		return pm++;
 	}
 	
+	/**
+	 * Seq for TF payments
+	 * @return
+	 */
 	public synchronized long nextTf(){
 		return tf++;
 	}
