287ec53be7a75a1fa16324958d6c4cc2f5d9d7ee
JBaba <naimish101.viradia@gmail.com>
2016-08-30 12:03:59 -0400

    First impl

--- LeetCodeSol/src/leetcode/sol/One_1_to_20/N_389.java
+++ LeetCodeSol/src/leetcode/sol/One_1_to_20/N_389.java
@@ -12,9 +12,18 @@ package leetcode.sol.One_1_to_20;
  */
 public class N_389 {
 
+	public char findTheDifference(String s, String t) {
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)!=t.charAt(i)){
+				return t.charAt(i);
+			}
+		}
+		return 0;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		N_389 n =new N_389();
+		System.out.println(n.findTheDifference("abc", "atbc"));	
 	}
 
 }
