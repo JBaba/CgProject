b7ce579ba524159f66ed26288eefcf70eeb10227
jbaba <naimish101.viradia@gmail.com>
2016-09-17 14:02:04 -0400

    Application init

---
+++ MultithreadingPoc/src/Application/Application.java
@@ -0,0 +1,17 @@
+package Application;
+
+/**
+ * Run Multi threading application
+ * @author jbaba
+ *
+ */
+public class Application {
+
+	public Application() {
+	}
+	
+	public static void main(String[] args) {
+		Application app = new Application();
+	}
+
+}
