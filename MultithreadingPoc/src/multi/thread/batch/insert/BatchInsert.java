c4bcef9d9856d98859950738333237bbbdfb254f
jbaba <naimish101.viradia@gmail.com>
2016-09-20 14:02:36 -0400

    fs paraller insert added

--- MultithreadingPoc/src/multi/thread/batch/insert/BatchInsert.java
+++ MultithreadingPoc/src/multi/thread/batch/insert/BatchInsert.java
@@ -1,5 +1,44 @@
 package multi.thread.batch.insert;
 
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Insert operations
+ * @author jbaba
+ *
+ */
 public class BatchInsert {
+	int count = 0;
+	public BatchInsert(int count) {
+		this.count = count;
+	}
+	
+	public void run(){
+		CountDownLatch fsCountDown = new CountDownLatch(count);
+		FSInsert fsInsert = new FSInsert(count, fsCountDown);
+		
+		
+	}
+}
+
+/**
+ * FSInsert Parallel insert
+ * @author jbaba
+ *
+ */
+class FSInsert implements Runnable{
+
+	int size = 0;
+	CountDownLatch fsCountDown;
+	
+	public FSInsert(int size,CountDownLatch CountDown ) {
+		this.size=size;
+		this.fsCountDown=CountDown;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
 	
 }
