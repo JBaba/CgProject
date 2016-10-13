7879cffffa7496179805beaa28cd0eb04b18ead2
JBaba <naimish101.viradia@gmail.com>
2016-10-13 12:22:21 -0400

    Recusion started

---
+++ LeetCodeSol/src/leetcode/sol/recursive/FirstRecusive.java
@@ -0,0 +1,23 @@
+package leetcode.sol.recursive;
+
+/**
+ * This class contains easy recursive problems and solutions
+ * @author nviradia
+ *
+ */
+public class FirstRecusive {
+
+	/**
+	 * Fibonacci rec solution
+	 * @param n
+	 * @return
+	 */
+	public static int fib(int n){
+		return (n<=1)?n:(n-1)+(n-2);
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
