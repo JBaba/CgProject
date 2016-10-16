ccbbf5074e6d9f5c52176e992463d91197cd5517
JBaba <naimish101.viradia@gmail.com>
2016-10-16 15:09:33 -0400

    String mingle string buffer added

--- LeetCodeSol/src/leetcode/sol/recursive/StringMingling.java
+++ LeetCodeSol/src/leetcode/sol/recursive/StringMingling.java
@@ -28,10 +28,10 @@ public class StringMingling {
 	 * @param s2
 	 * @return
 	 */
-	public static String mingle(String s1,String s2){
+	public static StringBuffer mingle(String s1,String s2){
 		if(s1.length()==0)
-			return "";
-		return s1.charAt(0)+""+s2.charAt(0)+""+mingle(s1.substring(1), s2.substring(1));
+			return new StringBuffer();
+		return (new StringBuffer()).append(s1.charAt(0)).append(s2.charAt(0)).append(mingle(s1.substring(1), s2.substring(1)));
 	}
 	
 	public static void main(String[] args) {
