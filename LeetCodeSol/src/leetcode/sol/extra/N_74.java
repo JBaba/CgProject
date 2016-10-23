639e94a20fd77b0a9e43b7fd71f2d4eb5b80d3d3
JBaba <naimish101.viradia@gmail.com>
2016-10-23 14:43:24 -0400

    leetcode matrix 2d I added with recursion

--- LeetCodeSol/src/leetcode/sol/extra/N_74.java
+++ LeetCodeSol/src/leetcode/sol/extra/N_74.java
@@ -22,13 +22,55 @@ package leetcode.sol.extra;
 public class N_74 {
 	public N_74() {
 		
-		int[][] maze = {{1,   3,  5,  7},
-				  		{10, 11, 16, 20},
-				  		{23, 30, 34, 50}};
+		int[][] maze = new int[8][];
 		
-		searchMatrix(maze, 30);
+		for(int i = 0;i<maze.length;i++){
+			if(i!=5){
+				maze[i] = new int[10];
+				for (int j = 0; j < 10; j++) {
+					maze[i][j] = ((i*100000)+j);
+				}
+			}
+			else {
+				maze[i] = new int[100000];
+				for (int j = 0; j < 100000; j++) {
+					maze[i][j] = ((i*100000)+j);
+				}
+			}
+		}
+		
+		int target = 599999;
+		
+		long start = System.currentTimeMillis();
+		searchMatrix(maze,target );
+		long end =  System.currentTimeMillis();
+		System.out.println((end-start));
+	
+		start = System.currentTimeMillis();
+		searchMatrixWithRec(maze, target, 0);
+		end =  System.currentTimeMillis();
+		System.out.println((end-start));
+		
 	}
 	
+	private boolean searchMatrixWithRec(int[][] matrix, int target, int row) {
+		if(row == matrix.length)
+			return false;
+		
+		if(matrix[row][0] <= target && matrix[row][matrix[row].length-1] >= target){
+			for (int j = 0; j < matrix[row].length; j++) {
+				if(matrix[row][j] == target){
+					System.out.println(row+" "+j);
+					return true;
+				}
+			}
+		}else{
+			searchMatrixWithRec(matrix, target, row+1);
+		}
+		
+		return false;
+	}
+
 	public boolean searchMatrix(int[][] matrix, int target) {
         
 		for (int i = 0; i < matrix.length; i++) {
