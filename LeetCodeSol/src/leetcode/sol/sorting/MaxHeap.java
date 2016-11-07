ee205cb1428352c4b9fabb074ec7f63f986dcc67
jbaba <naimish101.viradia@gmail.com>
2016-11-06 23:07:44 -0500

    heap - remove fun added

--- LeetCodeSol/src/leetcode/sol/sorting/MaxHeap.java
+++ LeetCodeSol/src/leetcode/sol/sorting/MaxHeap.java
@@ -7,22 +7,75 @@ public class MaxHeap {
 	int[] ary;
 	int size = 0;
 	int MAX_SIZE = 0;
+	boolean isPrint = false;
 	
 	public MaxHeap(int size) {
 		ary = new int[size];
 		MAX_SIZE = size;
 		
-		add(25);
-		add(40);
-		add(24);
-		add(30);
-		add(32);
-		add(35);
-		add(9);
+		add(25); add(40); add(24); add(30); add(32); add(35); add(9);
+		add(15); add(4); add(14); add(33); add(19); add(28); add(29);
+		add(7); add(6); add(5); add(3); add(2); add(31); 
+		
+		isPrint = true;
+		add(39);
+		
+		extractMax();
+		
+		remove(8);
 	}
 	
+	private void remove(int i) {
+		System.out.println("Removing "+i+" position :"+ary[i]);
+		ary[i] = Integer.MAX_VALUE;
+		
+		print();
+		siftUp(i);
+		extractMax();
+		print();
+	}
+
+	private void extractMax() {
+		if(size == 0){
+			System.out.println("Heap is empty.");
+			return;
+		}
+		System.out.println("Max value:"+ary[0]);
+		
+		ary[0] = ary[size-1];
+		ary[size-1] = 0;
+		--size;
+		
+		print();
+		siftDown(0);
+		print();
+	}
+
+	private void siftDown(int i) {
+		int right = getRightChild(i+1)-1;
+		int left = getLeftChild(i+1)-1;
+		
+		if(ary[right] > ary[left]){
+			if(ary[right]>ary[i]){
+				swap(right, i);
+				siftDown(right);
+			}else{
+				return;
+			}
+		}
+		else{
+			if(ary[left]>ary[i]){
+				swap(left, i);
+				siftDown(left);
+			}else{
+				return;
+			}
+		}
+	}
+
 	private void print() {
-		System.out.println(Arrays.toString(ary));
+		if(isPrint)
+			System.out.println(Arrays.toString(ary));
 	}
 
 	private void add(int i) {
@@ -53,27 +106,33 @@ public class MaxHeap {
 		
 		int parent = getParent(i);
 		if(ary[parent] < ary[i]){
-			int temp = ary[parent];
-			ary[parent] = ary[i];
-			ary[i] = temp;
+			swap(parent,i);
 			siftUp(parent);
 		}		
 	}
 	
+	
+	
+	private void swap(int parent, int i) {
+		int temp = ary[parent];
+		ary[parent] = ary[i];
+		ary[i] = temp;
+	}
+
 	private int getParent(int i){
 		return (i%2==0)?(i/2)-1:(i/2);
 	}
 	
 	private int getRightChild(int i){
-		return 2*i;
+		return 2*i + 1;
 	}
 	
 	private int getLeftChild(int i){
-		return (2*i) + 1;
+		return (2*i);
 	}
 
 	public static void main(String[] args) {
-		MaxHeap mh = new MaxHeap(20);
+		MaxHeap mh = new MaxHeap(30);
 	}
 
 }
