1207e6bd405913b4e36b7f4e1d2f62cc4fe0ea60
JBaba <naimish101.viradia@gmail.com>
2017-02-10 16:33:46 -0500

    median

--- LeetCodeSol/src/com/amazon/Median_266.java
+++ LeetCodeSol/src/com/amazon/Median_266.java
@@ -3,8 +3,24 @@ package com.amazon;
 public class Median_266 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Median_266 md = new Median_266();
+		int[] ary = {1,2,3,4,5,6};
+		printMedian(ary);
+	}
+
+	private static void printMedian(int[] ary) {
+
+		int median;
+		int mid = ary.length / 2;
+		
+		if(ary.length % 2 == 0){
+			median = ( ary[mid-1] + ary[mid] ) / 2;
+		}else{
+			median = ( ary[mid] );
+		}
+
+		System.out.println("Median: "+median);
 	}
 
 }
