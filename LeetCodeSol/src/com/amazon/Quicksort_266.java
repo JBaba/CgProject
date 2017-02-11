0a3b297011e683d7943626faa0b5056e5b5d501c
JBaba <naimish101.viradia@gmail.com>
2017-02-11 12:57:32 -0500

    quick sort

--- LeetCodeSol/src/com/amazon/Quicksort_266.java
+++ LeetCodeSol/src/com/amazon/Quicksort_266.java
@@ -1,18 +1,52 @@
 package com.amazon;
 
+import java.util.Arrays;
+
 public class Quicksort_266 {
 
 	public static void main(String[] args) {
-		int[] input = {10, 80, 30, 90, 40, 50, 70};
 		
 		Quicksort_266 qc = new Quicksort_266();
-		qc.quicksort();
+		
+		int[] input = {90, 80, 70, 60, 50, 40, 30};
+		input = qc.quicksort(input,0,input.length-1);
+		System.out.println(Arrays.toString(input));
+		
+		int[] input2 = {10, 80, 30, 90, 40, 50, 70};
+		input2 = qc.quicksort(input2,0,input2.length-1);
+		System.out.println(Arrays.toString(input2));
 		
 	}
 
-	private void quicksort() {
-		// TODO Auto-generated method stub
+	private int[] quicksort(int[] input, int l, int h) {
+		if(l<h){
+			int partitionIndex = partition(input,l,h);
+			
+			quicksort(input, l, partitionIndex-1);
+			quicksort(input, partitionIndex+1, h);
+		}
+		return input;
+	}
+
+	private int partition(int[] input, int l, int h) {
+		int pivot = input[h];
 		
+		// start with low
+		int i=(l-1);
+		
+		for (int j = l; j <= h; j++) {
+			if(input[j] <= pivot){
+				i++;
+				switchEle(input,i,j);
+			}
+		}
+		return i;
+	}
+	
+	private void switchEle(int[] input, int i, int j) {
+		int temp = input[i];
+		input[i] = input[j];
+		input[j]=temp;
 	}
 
 }
